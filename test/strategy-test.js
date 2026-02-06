7e3d748eb24dec6252cd5673eba1d78bac9afbec
Damian Krzeminski <pirxpilot@furkot.com>
2026-02-06 10:08:09 -0700

    simplify test

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,5 +1,7 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
 const TripItStrategy = require('../lib/passport-tripit/strategy.js');
 
 test('TripItStrategy', async t => {
@@ -26,42 +28,7 @@ test('TripItStrategy', async t => {
 
     // mock
     strategy._oauth.get = (_url, _token, _tokenSecret, callback) => {
-      const body =
-        '{ \
-            "timestamp": "1322517396", \
-            "num_bytes": "1240", \
-            "Profile": { \
-                "@attributes": { \
-                    "ref": "XXxxXxxXX-xXNx_XXNNNXx" \
-                }, \
-                "ProfileEmailAddresses": { \
-                    "ProfileEmailAddress": [{ \
-                        "address": "jaredhanson@example.com", \
-                        "is_auto_import": "false", \
-                        "is_confirmed": "true", \
-                        "is_primary": "true" \
-                    }, \
-                    { \
-                        "address": "jaredhanson@example.net", \
-                        "is_auto_import": "false", \
-                        "is_confirmed": "true", \
-                        "is_primary": "false" \
-                    }] \
-                }, \
-                "is_client": "true", \
-                "is_pro": "false", \
-                "screen_name": "jaredhanson", \
-                "public_display_name": "Jared Hanson", \
-                "profile_url": "people/jaredhanson", \
-                "home_city": "Berkeley, CA", \
-                "photo_url": "http://static.tripit.com/uploads/images/6/6/9/669XXXXXXXXXXXXX.jpg", \
-                "activity_feed_url": "http://www.tripit.com/feed/activities/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/activities.atom", \
-                "alerts_feed_url": "http://www.tripit.com/feed/alerts/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/alerts.atom", \
-                "ical_url": "webcal://www.tripit.com/feed/ical/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/tripit.ics" \
-            } \
-        }';
-
-      callback(null, body, undefined);
+      fs.readFile(path.join(__dirname, 'fixtures', 'response.json'), 'utf-8', callback);
     };
 
     await t.test('when told to load user profile', async () => {
