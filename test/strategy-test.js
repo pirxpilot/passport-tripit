531e0487875382452995ed581a7c8b94ab12bd21
Damian Krzeminski <pirxpilot@furkot.com>
2026-02-06 10:04:03 -0700

    use `biome` as linter

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -2,25 +2,32 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const TripItStrategy = require('../lib/passport-tripit/strategy.js');
 
-test('TripItStrategy', async (t) => {
+test('TripItStrategy', async t => {
   await t.test('strategy should be named tripit', () => {
-    const strategy = new TripItStrategy({
-      consumerKey: 'ABC123',
-      consumerSecret: 'secret'
-    }, function() {});
+    const strategy = new TripItStrategy(
+      {
+        consumerKey: 'ABC123',
+        consumerSecret: 'secret'
+      },
+      () => {}
+    );
 
     assert.equal(strategy.name, 'tripit');
   });
 
-  await t.test('strategy when loading user profile', async (t) => {
-    const strategy = new TripItStrategy({
-      consumerKey: 'ABC123',
-      consumerSecret: 'secret'
-    }, function() {});
+  await t.test('strategy when loading user profile', async t => {
+    const strategy = new TripItStrategy(
+      {
+        consumerKey: 'ABC123',
+        consumerSecret: 'secret'
+      },
+      () => {}
+    );
 
     // mock
-    strategy._oauth.get = function(url, token, tokenSecret, callback) {
-      const body = '{ \
+    strategy._oauth.get = (_url, _token, _tokenSecret, callback) => {
+      const body =
+        '{ \
             "timestamp": "1322517396", \
             "num_bytes": "1240", \
             "Profile": { \
@@ -45,12 +52,12 @@ test('TripItStrategy', async (t) => {
                 "is_pro": "false", \
                 "screen_name": "jaredhanson", \
                 "public_display_name": "Jared Hanson", \
-                "profile_url": "people\/jaredhanson", \
+                "profile_url": "people/jaredhanson", \
                 "home_city": "Berkeley, CA", \
-                "photo_url": "http:\/\/static.tripit.com\/uploads\/images\/6\/6\/9\/669XXXXXXXXXXXXX.jpg", \
-                "activity_feed_url": "http:\/\/www.tripit.com\/feed\/activities\/private\/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\/activities.atom", \
-                "alerts_feed_url": "http:\/\/www.tripit.com\/feed\/alerts\/private\/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\/alerts.atom", \
-                "ical_url": "webcal:\/\/www.tripit.com\/feed\/ical\/private\/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\/tripit.ics" \
+                "photo_url": "http://static.tripit.com/uploads/images/6/6/9/669XXXXXXXXXXXXX.jpg", \
+                "activity_feed_url": "http://www.tripit.com/feed/activities/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/activities.atom", \
+                "alerts_feed_url": "http://www.tripit.com/feed/alerts/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/alerts.atom", \
+                "ical_url": "webcal://www.tripit.com/feed/ical/private/XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/tripit.ics" \
             } \
         }';
 
@@ -84,14 +91,17 @@ test('TripItStrategy', async (t) => {
     });
   });
 
-  await t.test('strategy when loading user profile and encountering an error', async (t) => {
-    const strategy = new TripItStrategy({
-      consumerKey: 'ABC123',
-      consumerSecret: 'secret'
-    }, function() {});
+  await t.test('strategy when loading user profile and encountering an error', async t => {
+    const strategy = new TripItStrategy(
+      {
+        consumerKey: 'ABC123',
+        consumerSecret: 'secret'
+      },
+      () => {}
+    );
 
     // mock
-    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+    strategy._oauth.get = (_url, _token, _tokenSecret, callback) => {
       callback(new Error('something went wrong'));
     };
 
